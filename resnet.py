b46e584b6d046cde35c449ba5dca6ffd17cdc02d
Rosario <vellaro98@gmail.com>
2021-06-29 22:24:08 +0200

    ordnung der Modelle ???

--- resnet.py
+++ resnet.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 
 #transform like epretrained model
 from torchvision import transforms
+from torchvision.transforms.functional import crop
 
 transform = transforms.Compose([            
             transforms.Resize(256),                    
@@ -48,6 +49,121 @@ dataloaders = {
     "TESTCIFAR100" : testsetCIFAR100,
 }
 
+# %%
+# Uncertainty LOSSES
+import torch.nn.functional as F
+
+def relu_evidence(y):
+    return F.relu(y)
+
+
+def exp_evidence(y):
+    return torch.exp(torch.clamp(y, -10, 10))
+
+
+def softplus_evidence(y):
+    return F.softplus(y)
+
+#The Kullback-Leibler Divergence score, or KL divergence score,
+#quantifies how much one probability distribution differs from 
+#another probability distribution.
+def kl_divergence(alpha, num_classes, device=None):
+    if not device:
+        device = get_device()
+    beta = torch.ones([1, num_classes], dtype=torch.float32, device=device)
+    #Sum dirchlet distribution
+    S_alpha = torch.sum(alpha, dim=1, keepdim=True)
+    #Sum (number of labers)
+    S_beta = torch.sum(beta, dim=1, keepdim=True)
+    # ln (gammafunct) | gammafunc = (n-1)!
+    lnB = torch.lgamma(S_alpha) -         torch.sum(torch.lgamma(alpha), dim=1, keepdim=True)
+    # ist das  nicht immer 0? | torch.sum(torch.lgamma(beta)
+    lnB_uni = torch.sum(torch.lgamma(beta), dim=1,
+                        keepdim=True) - torch.lgamma(S_beta)
+    #ableitung gammafunc(x) / gammafunc(x)
+    dg0 = torch.digamma(S_alpha)
+    dg1 = torch.digamma(alpha)
+    # sum( dirchlet - 1 *(digamma(Sum_alpha) -(digamma(alpha)) ) 
+    # + ???  
+    kl = torch.sum((alpha - beta) * (dg1 - dg0), dim=1,
+                   keepdim=True) + lnB + lnB_uni
+    return kl
+
+#???
+def loglikelihood_loss(y, alpha, device=None):
+    if not device:
+        device = get_device()
+    y = y.to(device)
+    alpha = alpha.to(device)
+    S = torch.sum(alpha, dim=1, keepdim=True)
+    
+    loglikelihood_err = torch.sum(
+        (y - (alpha / S)) ** 2, dim=1, keepdim=True)
+    
+    loglikelihood_var = torch.sum(
+        alpha * (S - alpha) / (S * S * (S + 1)), dim=1, keepdim=True)
+    loglikelihood = loglikelihood_err + loglikelihood_var
+    return loglikelihood
+#???
+def mse_loss(y, alpha, epoch_num, num_classes, annealing_step, device=None):
+    if not device:
+        device = get_device()
+    y = y.to(device)
+    alpha = alpha.to(device)
+    loglikelihood = loglikelihood_loss(y, alpha, device=device)
+
+    annealing_coef = torch.min(torch.tensor(
+        1.0, dtype=torch.float32), torch.tensor(epoch_num / annealing_step, dtype=torch.float32))
+
+    kl_alpha = (alpha - 1) * (1 - y) + 1
+    kl_div = annealing_coef *         kl_divergence(kl_alpha, num_classes, device=device)
+    return loglikelihood + kl_div
+
+# EQ 4 mit func = digamma
+def edl_loss(func, y, alpha, epoch_num, num_classes, annealing_step, device=None):
+    y = y.to(device)
+    alpha = alpha.to(device)
+    S = torch.sum(alpha, dim=1, keepdim=True)
+
+    A = torch.sum(y * (func(S) - func(alpha)), dim=1, keepdim=True)
+
+    annealing_coef = torch.min(torch.tensor(
+        1.0, dtype=torch.float32), torch.tensor(epoch_num / annealing_step, dtype=torch.float32))
+
+    kl_alpha = (alpha - 1) * (1 - y) + 1
+    kl_div = annealing_coef *         kl_divergence(kl_alpha, num_classes, device=device)
+    return A + kl_div
+
+
+def edl_mse_loss(output, target, epoch_num, num_classes, annealing_step, device=None):
+    if not device:
+        device = get_device()
+    evidence = relu_evidence(output)
+    alpha = evidence + 1
+    loss = torch.mean(mse_loss(target, alpha, epoch_num,
+                               num_classes, annealing_step, device=device))
+    return loss
+
+
+def edl_log_loss(output, target, epoch_num, num_classes, annealing_step, device=None):
+    if not device:
+        device = get_device()
+    evidence = relu_evidence(output)
+    alpha = evidence + 1
+    loss = torch.mean(edl_loss(torch.log, target, alpha,
+                               epoch_num, num_classes, annealing_step, device))
+    return loss
+
+
+def edl_digamma_loss(output, target, epoch_num, num_classes, annealing_step, device=None):
+    if not device:
+        device = get_device()
+    evidence = relu_evidence(output)
+    alpha = evidence + 1
+    loss = torch.mean(edl_loss(torch.digamma, target, alpha,
+                               epoch_num, num_classes, annealing_step, device))
+    return loss
+
 
 # %%
 #Load pretrained Model
@@ -74,14 +190,14 @@ print(resnet18)
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-batch = next(iter(dataloaders['train']))
-print(batch[0].shape)
-plt.imshow(batch[0][0].permute(1, 2, 0)) # image an der ersten stelle
-print(batch[1][0]) # label an der ersten stelle
-plt.show()
-
+#
+#
+#batch = next(iter(dataloaders['train']))
+#print(batch[0].shape)
+#plt.imshow(batch[0][0].permute(1, 2, 0)) # image an der ersten stelle
+#print(batch[1][0]) # label an der ersten stelle
+#plt.show()
+#
 
 # %%
 
@@ -241,8 +357,8 @@ def train_model(model, dataloaders, criterion, optimizer, device, num_classes = 
         evidence_history.append(epoch_evidence1)
 
         # speichert jede Epoche
-        torch.save(model.state_dict(), os.path.join('./results/models', '{0:0=2d}.pth'.format(epoch)))
-        print(f"Saved: ./results/models"+ '{0:0=2d}.pth'.format(epoch))
+        torch.save(model.state_dict(), os.path.join('./results/models', str_criterion , '{0:0=2d}.pth'.format(epoch)))
+        print(f"Saved: ./results/models/" + str_criterion + '{0:0=2d}.pth'.format(epoch))
         print()
 
     time_elapsed = time.time() - since
@@ -268,121 +384,6 @@ welche layer sollen trainiert werden
 #optimizer = Adam(params_to_update)
 optimizer = Adam(resnet18.parameters())
 
-# %%
-# Uncertainty LOSSES
-import torch.nn.functional as F
-
-def relu_evidence(y):
-    return F.relu(y)
-
-
-def exp_evidence(y):
-    return torch.exp(torch.clamp(y, -10, 10))
-
-
-def softplus_evidence(y):
-    return F.softplus(y)
-
-#The Kullback-Leibler Divergence score, or KL divergence score,
-#quantifies how much one probability distribution differs from 
-#another probability distribution.
-def kl_divergence(alpha, num_classes, device=None):
-    if not device:
-        device = get_device()
-    beta = torch.ones([1, num_classes], dtype=torch.float32, device=device)
-    #Sum dirchlet distribution
-    S_alpha = torch.sum(alpha, dim=1, keepdim=True)
-    #Sum (number of labers)
-    S_beta = torch.sum(beta, dim=1, keepdim=True)
-    # ln (gammafunct) | gammafunc = (n-1)!
-    lnB = torch.lgamma(S_alpha) -         torch.sum(torch.lgamma(alpha), dim=1, keepdim=True)
-    # ist das  nicht immer 0? | torch.sum(torch.lgamma(beta)
-    lnB_uni = torch.sum(torch.lgamma(beta), dim=1,
-                        keepdim=True) - torch.lgamma(S_beta)
-    #ableitung gammafunc(x) / gammafunc(x)
-    dg0 = torch.digamma(S_alpha)
-    dg1 = torch.digamma(alpha)
-    # sum( dirchlet - 1 *(digamma(Sum_alpha) -(digamma(alpha)) ) 
-    # + ???  
-    kl = torch.sum((alpha - beta) * (dg1 - dg0), dim=1,
-                   keepdim=True) + lnB + lnB_uni
-    return kl
-
-#???
-def loglikelihood_loss(y, alpha, device=None):
-    if not device:
-        device = get_device()
-    y = y.to(device)
-    alpha = alpha.to(device)
-    S = torch.sum(alpha, dim=1, keepdim=True)
-    
-    loglikelihood_err = torch.sum(
-        (y - (alpha / S)) ** 2, dim=1, keepdim=True)
-    
-    loglikelihood_var = torch.sum(
-        alpha * (S - alpha) / (S * S * (S + 1)), dim=1, keepdim=True)
-    loglikelihood = loglikelihood_err + loglikelihood_var
-    return loglikelihood
-#???
-def mse_loss(y, alpha, epoch_num, num_classes, annealing_step, device=None):
-    if not device:
-        device = get_device()
-    y = y.to(device)
-    alpha = alpha.to(device)
-    loglikelihood = loglikelihood_loss(y, alpha, device=device)
-
-    annealing_coef = torch.min(torch.tensor(
-        1.0, dtype=torch.float32), torch.tensor(epoch_num / annealing_step, dtype=torch.float32))
-
-    kl_alpha = (alpha - 1) * (1 - y) + 1
-    kl_div = annealing_coef *         kl_divergence(kl_alpha, num_classes, device=device)
-    return loglikelihood + kl_div
-
-# EQ 4 mit func = digamma
-def edl_loss(func, y, alpha, epoch_num, num_classes, annealing_step, device=None):
-    y = y.to(device)
-    alpha = alpha.to(device)
-    S = torch.sum(alpha, dim=1, keepdim=True)
-
-    A = torch.sum(y * (func(S) - func(alpha)), dim=1, keepdim=True)
-
-    annealing_coef = torch.min(torch.tensor(
-        1.0, dtype=torch.float32), torch.tensor(epoch_num / annealing_step, dtype=torch.float32))
-
-    kl_alpha = (alpha - 1) * (1 - y) + 1
-    kl_div = annealing_coef *         kl_divergence(kl_alpha, num_classes, device=device)
-    return A + kl_div
-
-
-def edl_mse_loss(output, target, epoch_num, num_classes, annealing_step, device=None):
-    if not device:
-        device = get_device()
-    evidence = relu_evidence(output)
-    alpha = evidence + 1
-    loss = torch.mean(mse_loss(target, alpha, epoch_num,
-                               num_classes, annealing_step, device=device))
-    return loss
-
-
-def edl_log_loss(output, target, epoch_num, num_classes, annealing_step, device=None):
-    if not device:
-        device = get_device()
-    evidence = relu_evidence(output)
-    alpha = evidence + 1
-    loss = torch.mean(edl_loss(torch.log, target, alpha,
-                               epoch_num, num_classes, annealing_step, device))
-    return loss
-
-
-def edl_digamma_loss(output, target, epoch_num, num_classes, annealing_step, device=None):
-    if not device:
-        device = get_device()
-    evidence = relu_evidence(output)
-    alpha = evidence + 1
-    loss = torch.mean(edl_loss(torch.digamma, target, alpha,
-                               epoch_num, num_classes, annealing_step, device))
-    return loss
-
 
 # %%
 device = get_device()
@@ -390,6 +391,7 @@ device = get_device()
 # Setup the loss function
 #verschidene kriterien
 criterion = nn.CrossEntropyLoss()
+str_criterion = "CrossEntropyLoss"
 #criterion = nn.CrossEntropyLoss()
 #criterion = nn.CrossEntropyLoss()
 #criterion = nn.CrossEntropyLoss()
@@ -428,7 +430,7 @@ def eval_model(model, dataloaders, device, num_classes =10):
     best_acc = 0.0
     best_evidence = 0.0
 
-    saved_models = glob.glob('./results/models/' + '*.pth')
+    saved_models = glob.glob('./results/models/' + str_criterion + '*.pth')
     saved_models.sort()
     print('saved_model', saved_models)
 
@@ -468,7 +470,7 @@ def eval_model(model, dataloaders, device, num_classes =10):
         epoch_acc = running_corrects.double() / len(dataloaders.dataset)
         epoch_evidence1 = mean_evidence 
         print('Acc: {:.4f}'.format(epoch_acc))
-        
+        print('Acc: {:.4f}'.format(epoch_evidence1))
         if epoch_acc > best_acc:
             best_acc = epoch_acc
 
@@ -490,7 +492,7 @@ def eval_model(model, dataloaders, device, num_classes =10):
 
 
 val_acc_hist = eval_model(resnet18, dataloaders["val"], device, num_classes=10)
-val_acc_hist = eval_model(resnet18, dataloaders["TESTCIFAR100"], device, num_classes=100)
+
 
 
 # %%
@@ -499,4 +501,6 @@ plt.plot(train_acc_hist)
 plt.plot(val_acc_hist)
 plt.plot(train_evidence_hist)
 plt.plot(train_loss_hist)
-plt.savefig("./results/models/TrainingHist.png")
+plt.savefig('./results/models/' + str_criterion + 'TrainHisto')
+
+val_acc_hist = eval_model(resnet18, dataloaders["TESTCIFAR100"], device, num_classes=100)
