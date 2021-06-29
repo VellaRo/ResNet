bbc0c698a2a3a9f60fb4e360c998fde27b790cc4
Rosario <vellaro98@gmail.com>
2021-06-29 18:36:39 +0200

    test

--- resnet.py
+++ resnet.py
@@ -142,6 +142,8 @@ import time
 import os
 def train_model(model, dataloaders, criterion, optimizer, device, num_classes = 10, num_epochs=25, is_train=True, uncertainty=False):
     print("im using:" + str(device)) # see if using GPU cuda
+    print(torch.cudaGetDeviceCount())
+    print(torch.NumCudaDevices())
     since = time.time()
     
     acc_history = []
