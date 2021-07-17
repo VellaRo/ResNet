ecfb61f5b40d19b9a0cddc1a9be159ce641a62c4
rosario <r.vella@hotmail.de>
2021-07-17 13:29:42 +0200

    first test Dataset Class

--- notebooks/exploringCIFAR10_100.py
+++ notebooks/exploringCIFAR10_100.py
@@ -1,7 +1,10 @@
 import torch
+from torch.functional import split
+from torch.utils.data.dataset import Dataset
 import torchvision
 #transform like epretrained model
 from torchvision import transforms
+import numpy as np
 
 transform = transforms.Compose([            
             transforms.Resize(256),                    
@@ -30,9 +33,10 @@ trainloaderCIFAR10 = torch.utils.data.DataLoader(trainsetCIFAR10, batch_size=bat
 testloaderCIFAR10 = torch.utils.data.DataLoader(testsetCIFAR10, batch_size=batch_size,
                                          shuffle=False, num_workers=2)
 
+
 # CIFAR100
-trainsetCIFAR100 = torchvision.dataset.CIFAR100(root='./data', train=True,download=True, transform=transform)
-testsetCIFAR100 = torchvision.datasets.CIFAR100(root='./data', train=False,download=True, transform=transform)
+trainsetCIFAR100 = torchvision.datasets.CIFAR100(root='./data', train=True,download=True, transform=transform)
+testsetCIFAR100 =  torchvision.datasets.CIFAR100(root='./data', train=False,download=True, transform=transform)
 
 trainloaderCIFAR100 = torch.utils.data.DataLoader(trainsetCIFAR100,            
                             batch_size=batch_size,shuffle=True, num_workers=2)
@@ -40,28 +44,82 @@ trainloaderCIFAR100 = torch.utils.data.DataLoader(trainsetCIFAR100,
 testloaderCIFAR100 = torch.utils.data.DataLoader(testsetCIFAR100,            
                             batch_size=batch_size,shuffle=False, num_workers=2)
 
+#trainsetCIFAR100["coarse_labels"]
 
-## CIFAR90
-#def get_indices(dataset,class_labels_list):
-#    indices =  []
-#    for i in range(len(dataset.targets)):
-#        if dataset.targets[i] in class_labels_list:
-#            pass
-#        else:
-#            indices.append(i)
-#    return indices
-#
-#
-#testCifar90 = torchvision.datasets.CIFAR100(root='./data', train=False, download =True
-#                           ,transform=transform)
-#                           
-#class_labels_list = [0,1,2,3,4,5,6,7,8,9]
-#idx = get_indices(testCifar90, class_labels_list)
-#testloaderCIFAR90 = torch.utils.data.DataLoader(testCifar90,batch_size=batch_size, sampler = torch.utils.data.sampler.SubsetRandomSampler(idx))
+#for item in trainsetCIFAR100:
+#    print(item, type(trainsetCIFAR100[item]))
 
+import pickle
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        myDict = pickle.load(fo, encoding='latin1')
+    return myDict
+
+trainData = unpickle('./data/cifar-100-python/train')#type of items in each file
+testData = unpickle('./data/cifar-100-python/test')#type of items in each file
+
+
+labelsCoarse_TRAIN = trainData["coarse_labels"]
+labelsCoarse_TEST = testData["coarse_labels"]
+labelsFine_TRAIN = trainData["fine_labels"]
+labelsFine_TEST = testData["fine_labels"]
+
+dataTrain = trainData["data"]
+dataTest = testData["data"]
+
+
+print(type(dataTest)) # numpy array
+print(dataTest.shape)
+print(len(dataTest[0])) # 10000
+
+dataTest = dataTest.reshape(len(dataTest),3,32,32) #transpose ??? changes order
+dataTrain = dataTrain.reshape(len(dataTrain),3,32,32) 
+
+print(dataTest.shape)
+print(type(dataTest)) 
+print(len(dataTest[0])) 
+#for x in labelsCoarse_TEST:
+#    print(str(labelsCoarse_TEST[x]) +" "+ str(labelsFine_TEST[x])+ "\n")
+
+class DifferentLabelsCIFAR100(Dataset):
+    def __init__(self,  imgData, labels, transform=None):
+                    #root_dir,
+        ##
+        ##DO HERE THE PICKEL STUFF
+        ##
+        self.imgData = imgData
+       #self.root_dir = root_dir
+        self.labels = labels
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self, index):
+        img = self.imgData[index]
+        class_id = self.labels[index]
+        img = torch.tensor(img) 
+        class_id = torch.tensor(class_id)
+        
+        if self.transform != None:
+            transToPIL = transforms.ToPILImage()
+            img = transToPIL(img)
+            img = self.transform(img)
+             
+        return img ,class_id
+        
+
+coarseLabel_Dataset_Train = DifferentLabelsCIFAR100(dataTrain, labelsCoarse_TRAIN , transform)
+
+trainloaderCIFAR100Coarse = torch.utils.data.DataLoader(coarseLabel_Dataset_Train,            
+                            batch_size=batch_size,shuffle=True, num_workers=2)
+batch = next(iter(trainloaderCIFAR100Coarse))
+for x in range(len(batch[1])):
+    print(batch[0][x].size())
+    
 trainloaderCIFAR10.name = "CIFAR10_TRAIN"
 testloaderCIFAR10.name = "CIFAR10_TEST"
-#testloaderCIFAR90.name = "CIFAR90_TEST"
 testloaderCIFAR100.name = "CIFAR100_TRAIN"
 testloaderCIFAR100.name = "CIFAR100_TEST"
 CIFAR_dataloaders = {
@@ -69,8 +127,14 @@ CIFAR_dataloaders = {
     "CIFAR10_TEST": testloaderCIFAR10,
     "CIFAR100_Train" : trainloaderCIFAR100,
     "CIFAR100_TEST" : testloaderCIFAR100,
-    
-
 #    "CIFAR90_TEST" : testloaderCIFAR90
 }
 
+
+import matplotlib.pyplot as plt
+
+#data= next(iter(CIFAR_dataloaders["CIFAR100_Train"]))
+#grid_img = torchvision.utils.make_grid(data[0], nrow=5)
+#
+#plt.imshow(grid_img.permute(1, 2, 0)  )
+#plt.show()
